04e90c75a2283e0403260b49139f69a6f5d2b0fc
James Torio <32937709+jstorio@users.noreply.github.com>
2018-12-03 21:18:49 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,31 @@
+//initial groundwork for Password Generator
+
+function generatePassword() {
+    var length = 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    document.getElementById("generated").value = retVal;
+    copyText();
+    showCopiedSnackbar();
+    
+}
+
+function copyText() {
+	var text = document.getElementById("generated");
+    text.select();
+    document.execCommand("copy");
+}
+
+function showCopiedSnackbar() {
+    // Get the snackbar DIV
+    var x = document.getElementById("snackbar");
+
+    // Add the "show" class to DIV
+    x.className = "show";
+
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+} 
