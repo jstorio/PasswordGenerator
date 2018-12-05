346d8c25fd799a198ec7df9ad9931f0ca57baf86
James Torio <32937709+jstorio@users.noreply.github.com>
2018-12-04 20:31:28 -0500

    Add files via upload
    Updated generator to have 2 of each cases

--- javascript.js
+++ javascript.js
@@ -1,13 +1,32 @@
 //initial groundwork for Password Generator
 
 function generatePassword() {
+    
     var length = 8,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    	charsetLower = "abcdefghijklmnopqrstuvwxyz",
+        charsetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        charsetNum = "0123456789",
+        charsetSpec = "@%+?/'!#$^?:,()[]{}~-.",
         retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
+    for (var i = 0, n = charsetLower.length; i < 2; i++) {
+    	retVal += charsetLower.charAt(Math.floor(Math.random() * n));
+    	console.log(i);
     }
-    document.getElementById("generated").value = retVal;
+    for (var i = 0, n = charsetUpper.length; i < 2; i++) {
+    	retVal += charsetUpper.charAt(Math.floor(Math.random() * n));
+    	console.log(i);
+    }
+    for (var i = 0, n = charsetNum.length; i < 2; i++) {
+    	retVal += charsetNum.charAt(Math.floor(Math.random() * n));
+    	console.log(i);
+    }
+    for (var i = 0, n = charsetSpec.length; i < 2; i++) {
+    	retVal += charsetSpec.charAt(Math.floor(Math.random() * n));
+    	console.log(i);
+    }
+
+    var shuffled = retVal.split('').sort(function(){return 0.5-Math.random()}).join('');
+    document.getElementById("generated").value = shuffled;
     copyText();
     showCopiedSnackbar();
     
@@ -16,7 +35,7 @@ function generatePassword() {
 function copyText() {
 	var text = document.getElementById("generated");
     text.select();
-    document.execCommand("copy");
+    document.execCommand("copy"); 
 }
 
 function showCopiedSnackbar() {
@@ -27,5 +46,5 @@ function showCopiedSnackbar() {
     x.className = "show";
 
     // After 3 seconds, remove the show class from DIV
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 1500);
 } 
